756795897f54dcd1e570339a86b17336bc53a47d
yaman <yamantamrakar23@gmail.com>
2024-03-19 13:01:27 +0545

    add

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import HeroSection from "@/components/ui/HeroSection";
+import HeroSction from "@/components/ui/HeroSection";
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.13]">
+          <h1 className="text-2xl text-center text-white">code with ancient</h1>
+
+          <HeroSection />
+
+    </main>
+    );
+}
---
+++ src/components/ui/HeroSection.tsx
@@ -0,0 +1,9 @@
+
+
+function HeroSection() {
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex">HeroSection</div>
+  )
+}
+
+export default HeroSection
